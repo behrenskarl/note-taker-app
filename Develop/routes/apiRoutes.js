a6a35b15a8a56a672603068d895f893cd5377638
DAGDAD <behrens.karl@gmail.com>
2020-08-30 23:35:13 -0700

    deletes notes, adds id to notes, needs debugging within the post or delete api calls because it doesn't give every note an id and deletes multiple notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,30 +3,40 @@ const path = require("path");
 const notesdb = require(path.join(__dirname, '../db/db.json'));
 
 //assigns id to notesdb array
-let i = Math.random();
-notesdb.map(n => {
-    n['id'] = i;
-    i++;
-});
 
-module.exports = function (app) {
 
-    app.get('/api/notes', function (req, res) {
-    res.json(notesdb);
+module.exports = function(app) {
+
+
+    app.get('/api/notes', function(req, res) {
+        res.json(notesdb);
+        console.log('api get notes');
     });
 
 
-    app.post('/api/notes', function (req, res) {  
+    app.post('/api/notes', function(req, res) {  
     //res.json(db); might not be what's needed here
+        let i = Math.floor(Math.random() * 1000); 
+        notesdb.map(n => {
+            n['id'] = i;
+            i++;
+            console.log('giving api calls an id')
+        });
+    
         const note = req.body;
+
+
 
         notesdb.push(note);
         res.json(note);
 
         fs.writeFileSync(
             path.join(__dirname, '../db/db.json'),
-            JSON.stringify(notesdb)    
+            JSON.stringify(notesdb), null, 2
         );
+        console.log('api post note');
+
+
     });
 
 //API PUT TO ADD ID TO NOTES BEFORE DELETE
@@ -35,16 +45,26 @@ module.exports = function (app) {
 
 
 //API DELETE BASED ON /api/notes/:id
-    app.delete('/api/notes:id', function (req, res) {
-        notesdb.delete(req.body.id);
+    app.delete('/api/notes/:id', function (req, res) {
+        let grabID = req.params.id;
+        console.log(grabID);
+
+        function deleteID() {
+            for (let j = 0; j < notesdb.length; j++) {
+                if (notesdb[j].id = grabID) {
+                    notesdb.splice(j, 1);
+                    console.log(notesdb.id);
+                    fs.writeFileSync(
+                        path.join(__dirname, '../db/db.json'),
+                        JSON.stringify(notesdb), null, 2
+                    );
+                    
+                     
+                }
+            }
+        }
+        deleteID();
         res.json(notesdb);
+        console.log(notesdb);
     });
-
-//   app.post('/api/clear', function (req, res) {
-//     // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
-
-//     res.json({ ok: true });
-//   });
 };
